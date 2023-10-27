a7223f1a54e513207f1fc5e2324c8efc8fe32871
vanara <vanarait.hyd@gmail.com>
2023-10-28 01:30:28 +0530

    app

--- src/components/CareerSection.js
+++ src/components/CareerSection.js
@@ -40,9 +40,9 @@ function CareerSection() {
               <div className={`service-icon ${service.icon}`}></div>
               <h3>{service.title}</h3>
               <p>{service.description}</p>
-              <Link to={service.link} className="learn-more">
+              <p><Link to={service.link} className="learn-more">
                 Learn More
-              </Link>
+              </Link></p>
             </div>
           </div>
         ))}
