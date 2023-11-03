5c0d36b63db9d237672abba44e50a508195b9829
vanarait <vanarait.hyd@gmail.com>
2023-11-04 00:44:33 +0530

    done

--- src/components/TextSlider.js
+++ src/components/TextSlider.js
@@ -1,29 +1,52 @@
 import React from 'react';
 import { Carousel } from 'react-bootstrap';
-import JavaTraining from "./JavaTraining";
-import UiTraining from "./UiTraining";
-import AzureTraining from "./AzureTraining";
-import '../css/TextSlider.css'; // Import your CSS file
+import TrainingCourse from './TrainingCourse';
+import '../css/TextSlider.css';
+
+const trainingData = [
+  
+  {
+    title: 'JAVA Fullstack With Real Time Project in Cloud',
+    trainer: 'Mr. Ramesh',
+    date: '21th Nov 2023 @ 9am',
+    fee: '---',
+    zoomLink: 'https://www.vanarait.com',
+    contact: '9494494085',
+    customStyle: 'uiCourse',
+  },
+  
+  {
+    title: 'Data structures in Cloud',
+    trainer: 'Mr. Ramesh',
+    date: '21th Nov 2023 @ 6pm',
+    fee: '---',
+    zoomLink: 'https://www.vanarait.com',
+    contact: '9494494085',
+    customStyle: 'dataCourse',
+  },
+  
+  {
+    title: 'JAVA Fullstack With Real Time Project in Cloud',
+    trainer: 'Mr. Venkatesh',
+    date: '28th Nov 2023 @ 8am',
+    fee: '---',
+    zoomLink: 'https://www.vanarait.com',
+    contact: '9494494085',
+    customStyle: 'javaCourse',
+  },
+  // Add similar objects for Java and Azure training
+];
 
 const TextSlider = () => {
   return (
     <Carousel>
-      <Carousel.Item>
-        <div className="slide-content">
-        <AzureTraining />
-          
-        </div>
-      </Carousel.Item>
-      <Carousel.Item>
-        <div className="slide-content">
-        <UiTraining />
-        </div>
-      </Carousel.Item>
-      <Carousel.Item>
-        <div className="slide-content">
-        <JavaTraining />
-        </div>
-      </Carousel.Item>
+      {trainingData.map((training, index) => (
+        <Carousel.Item key={index}>
+          <div className="slide-content">
+            <TrainingCourse {...training} />
+          </div>
+        </Carousel.Item>
+      ))}
     </Carousel>
   );
 };
