1574fab7303b8ae67278d3ab2a01dbc69936ec5d
Venkatesh <vboyini1@gmail.com>
2023-11-05 15:55:31 +0530

    test commit

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,11 @@
-import React from 'react';
-import '../css/Footer.css'; // Import the CSS file for sty
-import WhatsAppChatbot from './WhatsAppChatbot';
+import React from "react";
+import "../css/Footer.css"; // Import the CSS file for sty
+import WhatsAppChatbot from "./WhatsAppChatbot";
 const Footer = () => {
   return (
     <footer className="footer">
       <WhatsAppChatbot />
-      <p>&copy; 2023 Your Company. All rights reserved.</p>
+      <p>&copy; 2023 Your Company. All rights reserved reserved.</p>
     </footer>
   );
 };
