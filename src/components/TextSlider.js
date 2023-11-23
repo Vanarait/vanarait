c5225fef313ea7cb94519ac45c817889565e2f10
Venkatesh <vboyini1@gmail.com>
2023-11-23 19:37:07 +0530

    Component Modified

--- src/components/TextSlider.js
+++ src/components/TextSlider.js
@@ -7,31 +7,37 @@ const trainingData = [
   {
     title: "UI Full Stack With Real Time Project in Cloud",
     trainer: "Mr. Ramesh",
-    date: "21th Nov 2023 @ 9am",
+    date: "21th Jan 2024 @ 9am",
     fee: "8000 INR",
     zoomLink: "https://www.vanarait.com",
     contact: "+91 9392926261",
     customStyle: "uiCourse",
+    bgColor:
+      "linear-gradient(89.9deg, rgb(208, 246, 255) 0.1%, rgb(255, 237, 237) 47.9%, rgb(255, 255, 231) 100.2%)",
   },
 
   {
     title: "Data structures and Design patterns in Java",
     trainer: "Mr. Ramesh",
-    date: "21th Nov 2023 @ 6pm",
+    date: "21th Jan 2024 @ 6pm",
     fee: "5000 INR",
     zoomLink: "https://www.vanarait.com",
     contact: "+91 9392926261",
     customStyle: "dataCourse",
+    bgColor:
+      " radial-gradient(circle at 50.3% 44.5%, rgb(116, 147, 179) 0%, rgb(62, 83, 104) 100.2%)",
   },
 
   {
     title: "JAVA Full Stack With Real Time Project in Cloud",
     trainer: "Mr. Venkatesh",
-    date: "28th Nov 2023 @ 8am",
+    date: "28th Jan 2024 @ 8am",
     fee: "12000 INR",
     zoomLink: "https://www.vanarait.com",
     contact: "+91 9392926261",
     customStyle: "javaCourse",
+    bgColor:
+      "radial-gradient(592px at 48.2% 50%, rgba(255, 255, 249, 0.6) 0%, rgb(160, 199, 254) 74.6%)",
   },
   // Add similar objects for Java and Azure training
 ];
@@ -41,7 +47,10 @@ const TextSlider = () => {
     <Carousel>
       {trainingData.map((training, index) => (
         <Carousel.Item key={index}>
-          <div className="slide-content bg-white">
+          <div
+            className="slide-content"
+            style={{ background: training.bgColor }}
+          >
             <TrainingCourse {...training} />
           </div>
         </Carousel.Item>
